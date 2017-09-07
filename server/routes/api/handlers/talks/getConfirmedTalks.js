2cbdd2043e6b0a93d99d269045311d05309bb2c8
Yasmina Gavaldà <y.gavalda@gmail.com>
2017-09-07 12:02:38 +0200

    Test confirmed talks

--- server/routes/api/handlers/talks/getConfirmedTalks.js
+++ server/routes/api/handlers/talks/getConfirmedTalks.js
@@ -2,9 +2,11 @@ const Talk = __require('/models/Talk')
 
 function getConfirmedTalks (req, res) {
   var { id } = req.params
-  Talk.find({ creator: id, available: false }, (_, user) => {
-    res.json(user)
-  })
+  Talk.find({ creator: id, available: false })
+  	.populate('creator', 'joiners')
+  	.then((user) => {
+	    res.json(user)
+	  })
 }
 
 module.exports = getConfirmedTalks
